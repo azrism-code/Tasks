083421e83318a02b4972ed8b84c079b71f888352
azrism-code <azrism@gmail.com>
2026-09-17 14:45:30 +0300

    Force PWA refresh for repaired build

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "azri-tasks-v3";
+const CACHE = "azri-tasks-v4";
 const FILES = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
