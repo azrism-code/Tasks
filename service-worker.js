282d7bde6b55d63a6b0e862b8f09e41e1297c383
azrism-code <azrism@gmail.com>
2026-09-17 18:30:56 +0300

    Refresh PWA cache for v1.3.1

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "azri-tasks-v8";
-const FILES = ["./", "./index.html", "./style.css?v=1.3.0", "./app.js?v=1.3.0", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "azri-tasks-v9";
+const FILES = ["./", "./index.html", "./style.css?v=1.3.1", "./app.js?v=1.3.1", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
 self.addEventListener("fetch", event => {
