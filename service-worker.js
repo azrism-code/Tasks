3d9288f2ef2eb19b7084f612785ae6558db8391a
azrism-code <azrism@gmail.com>
2026-09-21 11:17:21 +0300

    Refresh cache for validated v1.9.0 build

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "azri-tasks-v30";
-const FILES = ["./", "./index.html", "./style.css?v=1.9.0-94112c0e", "./app.js?v=1.9.0-2cb677b7", "./manifest.webmanifest?v=1.9.0", "./icon.svg?v=1.9.0", "./header-logo.svg?v=1.9.0"];
+const CACHE = "azri-tasks-v31";
+const FILES = ["./", "./index.html", "./style.css?v=1.9.0-94112c0e", "./app.js?v=1.9.0-85aab4c1", "./manifest.webmanifest?v=1.9.0", "./icon.svg?v=1.9.0", "./header-logo.svg?v=1.9.0"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
 self.addEventListener("fetch", event => {
